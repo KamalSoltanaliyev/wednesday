f4266ff45d39d61abd69ef5c32e9ddfb5b82d687
KamalSoltanaliyev <148267364+KamalSoltanaliyev@users.noreply.github.com>
2023-11-29 19:07:54 +0400

    Add files via upload

---
+++ wednesday/wednesday/GenericCustomList.cs
@@ -0,0 +1,78 @@
+﻿namespace wednesday
+{
+    public class GenericCustomList<T> where T : new()
+    {
+        public T[] array;
+        public int Count { get; set; }
+        public int Capacity { get; set; }  = 0;
+
+        public GenericCustomList()
+        {
+            
+        }
+
+        public GenericCustomList(int count, int capacity)
+        {
+            array = new T[0];
+            Count = count;
+            Capacity = capacity;
+        }
+
+        public void Add(T item)
+        { 
+            
+        }
+        public T Find(Predicate<T> match)
+        {
+            foreach (T item in array)
+            {
+                if (match(item))
+                    return item;
+            }
+            return default(T);
+        }
+
+        public GenericCustomList<T> FindAll(Predicate<T> match)
+        {
+            GenericCustomList<T> result = new GenericCustomList<T>();
+            foreach (T item in array)
+            {
+                if (match(item))
+                    result.Add(item);
+            }
+            return result;
+        }
+
+        public bool Contains(T item)
+        {
+            foreach (T i in array)
+            {
+                if (object.Equals(i, item))
+                    return true;
+            }
+            return false;
+        }
+
+        public bool Exists(Predicate<T> match)
+        {
+            foreach (T item in array)
+            {
+                if (match(item))
+                    return true;
+            }
+            return false;
+        }
+
+        public void Remove(T item)
+        {
+        //    int index = Array.IndexOf(array, item);
+        //    if (index != -1)
+        //    {
+        //        Array.Copy(array, index + 1, array, index, count - index - 1);
+        //        count--;
+        //    }
+        }
+    }
+
+}
+
---
+++ wednesday/wednesday/Program.cs
@@ -0,0 +1,17 @@
+﻿using wednesday;
+
+GenericCustomList<int> generic = new();
+
+//generic.Capacity = 1;
+
+
+//Console.WriteLine(generic.Capacity);
+//Console.ReadKey();
+
+generic.Add(1);
+generic.Add(2);
+generic.Add(3);
+generic.Add(4);
+generic.Add(5);
+Console.WriteLine(generic.Count);
+Console.WriteLine(generic.Capacity);
